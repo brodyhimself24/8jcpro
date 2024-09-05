f899a66a13ec2ff85547f98d16a7bf0eaa457475
brodyhimself24 <brodyhzt17@sina.com>
2024-09-05 21:27:20 +0800

    Add files via upload

---
+++ app111.py
@@ -0,0 +1,47 @@
+import numpy as np
+import streamlit as st
+from joblib import load
+from sklearn.preprocessing import StandardScaler
+
+# Streamlit界面
+def main():
+    st.title('健康指标预测')
+
+    # 创建文本输入框供用户输入特征数据
+    weight = st.number_input("请输入体重 (kg)", min_value=0, max_value=150, value=65)
+    height = st.number_input("请输入身高 (cm)", min_value=0, max_value=250, value=175)
+    age = st.number_input("请输入年龄", min_value=0, max_value=120, value=25)
+    ra = st.number_input("请输入RA值", min_value=0, max_value=500, value=100)
+    la = st.number_input("请输入LA值", min_value=0, max_value=500, value=100)
+    tr = st.number_input("请输入TR值", min_value=0, max_value=500, value=50)
+    rl = st.number_input("请输入RL值", min_value=0, max_value=500, value=120)
+    ll = st.number_input("请输入LL值", min_value=0, max_value=500, value=120)
+
+    # 构建特征数组
+    features = np.array([weight, height, age, ra, la, tr, rl, ll]).reshape(1, -1)
+
+    # 用户点击预测按钮
+    if st.button('进行预测'):
+        # 加载模型和标准化器
+        model = load('E:/机器学习源文件/项目6/heu.joblib')
+        scaler_X = load('E:/机器学习源文件/项目6/scaler_X.joblib')
+
+        # 使用加载的 StandardScaler 实例进行特征标准化
+        features_scaled = scaler_X.transform(features)
+
+        # 使用模型进行预测
+        prediction = model.predict(features_scaled)
+
+        # 显示预测结果
+        st.write("预测结果如下：")
+        labels = ['体脂率1', '肌肉1', '骨骼肌1', '水分量1', '蛋白质1', '无机盐1', '内脏脂肪1', '基础代谢1', '节段肌肉左臂1', '节段肌肉右臂1', '节段肌肉躯干1', '节段肌肉左腿1', '节段肌肉右腿1', '骨量1', '腰臀比1', '浮肿评估1']
+        # 检查 prediction 的形状
+        if len(prediction) > 0:
+            for i, label in enumerate(labels):
+                # 直接访问 prediction 的元素
+                st.write(f"{label}: {prediction[0][i]:.2f}")
+        else:
+            st.write("预测结果为空，请检查模型和输入数据。")
+
+if __name__ == "__main__":
+    main()
